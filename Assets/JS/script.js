a0bd7e2d2371092bd01ba9684f0714fc339ff21b
JoeBA <135997793+JoeBA-1@users.noreply.github.com>
2023-07-25 18:27:30 +0300

    Update script.js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -23,28 +23,49 @@ let users = [["admin01","123"],["admin02","234"],["admin03","345"]]
 //     alert(`You still have ${3-counter} attempts`)   
 // })
 
-loginBtn.addEventListener("click", function(){
-if(counter >=3){
-    alert(`Max limit attempts reached`)
-}else{
-    counter ++
-    // alert(`Incorrect Username or Password <br> You still have ${3-counter}`)
-    for(let i=0; i<users.length; i++){
-        if(userEl.value == users[i][0] && passEl.value == users[i][1]){
-            document.write(`<h1 style="text-align: center; color: red">Welcome ${userEl.value}</h1>`)
-            setTimeout(second_page,3000)
-            // if((userEl.value = true) && (passEl.value = true)){
-            //     document.write(`<h1 style="text-align: center; color: red">Welcome ${userEl.value}</h1>`)
-            // setTimeout(second_page,3000)
-            }else{
-                alert(`Incorrect Username or Password 
-You still have ${3-counter} atttempts`)
-                break
-            }
-        }
-    }
+// loginBtn.addEventListener("click", function(){
+// if(counter >=3){
+//     alert(`Max limit attempts reached`)
+// }else{
+//     counter ++
+//     // alert(`Incorrect Username or Password <br> You still have ${3-counter}`)
+//     for(let i=0; i<users.length; i++){
+//         if(userEl.value == users[i][0] && passEl.value == users[i][1]){
+//             document.write(`<h1 style="text-align: center; color: red">Welcome ${userEl.value}</h1>`)
+//             setTimeout(second_page,3000)
+//             // if((userEl.value = true) && (passEl.value = true)){
+//             //     document.write(`<h1 style="text-align: center; color: red">Welcome ${userEl.value}</h1>`)
+//             // setTimeout(second_page,3000)
+//             }else{
+//                 alert(`Incorrect Username or Password 
+// You still have ${3-counter} atttempts`)
+//                 break
+//             }
+//         }
+//     }
     
-})
+// })
+
+loginBtn.addEventListener("click", function(){
+
+    if(counter>=3){
+        alert(`Max limit attempts reached`)
+    }else if(userEl.value == "admin01" && passEl.value == "123"){
+        document.write(`<h1 style="text-align: center; color: red">Welcome ${userEl.value}</h1>`)
+        setTimeout(second_page,3000)
+    }else if(userEl.value == "admin02" && passEl.value == "234"){
+        document.write(`<h1 style="text-align: center; color: red">Welcome ${userEl.value}</h1>`)
+        setTimeout(second_page,3000)
+    }else if(userEl.value == "admin03" && passEl.value == "345"){
+        document.write(`<h1 style="text-align: center; color: red">Welcome ${userEl.value}</h1>`)
+        setTimeout(second_page,3000)
+    }else{
+        counter ++
+        alert(`Incorrect Username or Password 
+        You still have ${3-counter} atttempts`)
+    }
+    })
+    
 
 function second_page(){
     window.location.replace("list.html")
