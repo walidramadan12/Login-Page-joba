16312496b584e201a73443cd4dabf4233e4c2305
JoeBA <135997793+JoeBA-1@users.noreply.github.com>
2023-07-25 17:10:11 +0300

    Login Page
    Login Page

---
+++ Assets/JS/script.js
@@ -0,0 +1,53 @@
+let userEl = document.getElementById("User")
+let passEl = document.getElementById("Pass")
+let loginBtn = document.getElementById("Loginbtn")
+document.title = "Log In"
+let counter = 0
+let users = [["admin01","123"],["admin02","234"],["admin03","345"]]
+
+// loginBtn.addEventListener("click", function(){
+//     console.log(counter)
+//     if(counter >= 2){
+//         alert(`You have reached max attmpts`)
+//     }else{
+//         counter ++
+//         for(let i=0; i<users.length; i++){
+//             if(userEl.value == users[i][0] && passEl.value == users[i][1] ){
+            
+//                 document.write(`<h1 style="text-align: center; color: red">Welcome ${userEl.value}</h1>`)
+//                 setTimeout(second_page,3000)
+//             }
+//         }
+        
+//     }
+//     alert(`You still have ${3-counter} attempts`)   
+// })
+
+loginBtn.addEventListener("click", function(){
+if(counter >=3){
+    alert(`Max limit attempts reached`)
+}else{
+    counter ++
+    // alert(`Incorrect Username or Password <br> You still have ${3-counter}`)
+    for(let i=0; i<users.length; i++){
+        if(userEl.value == users[i][0] && passEl.value == users[i][1]){
+            document.write(`<h1 style="text-align: center; color: red">Welcome ${userEl.value}</h1>`)
+            setTimeout(second_page,3000)
+            // if((userEl.value = true) && (passEl.value = true)){
+            //     document.write(`<h1 style="text-align: center; color: red">Welcome ${userEl.value}</h1>`)
+            // setTimeout(second_page,3000)
+            }else{
+                alert(`Incorrect Username or Password 
+You still have ${3-counter} atttempts`)
+                break
+            }
+        }
+    }
+    
+})
+
+function second_page(){
+    window.location.replace("list.html")
+}          
+
+
---
+++ Assets/JS/script_list.js
@@ -0,0 +1,41 @@
+let stageEl = document.getElementById("stage")
+let fightBtnEl = document.getElementById("fightbtn")
+document.title = "Time To Fight"
+const fighters = [
+    "🐉", "🐥", "🐊", "💩","🦍", "🐢","🐩","🦀","🐝","🤖","🐘",
+    "🐸","🕷","🐆","🦕","🦁"];
+
+    
+    fightBtnEl.addEventListener("click", function(){
+    let char01 = Math.floor (Math.random(fighters) * fighters.length )
+    let char02 = Math.floor (Math.random(fighters) * fighters.length )
+    let diff = 0
+    stageEl.innerHTML = ` ${fighters[char01]} VS ${fighters[char02]}`
+    if (fighters[3] == fighters[char01] || fighters[3] == fighters[char02]){ 
+      stageEl.innerHTML += `<br> ${fighters[3]} is the winner`}else{
+      if(char01>char02){
+        diff = char01 - char02
+        stageEl.innerHTML += `<br> ${fighters[char01]} is the winner <br> ${fighters[char01]} win by ${diff} ponts`
+        if(diff>9){
+          fightBtnEl.style.backgroundColor="#0f5"
+        }else{
+            fightBtnEl.style.backgroundColor="#fff"
+          }
+      }else if(char01<char02){
+        diff = char02 - char01
+        stageEl.innerHTML += `<br> ${fighters[char02]} is the winner <br> ${fighters[char01]} win by ${diff} ponts`
+        if(diff>9){
+          fightBtnEl.style.backgroundColor="#0f5"
+        }else{
+          fightBtnEl.style.backgroundColor="#fff"
+        }
+      }else{
+        stageEl.innerHTML += `<br> Tie Game`
+        fightBtnEl.style.backgroundColor="#f0f"
+      }
+      }})
+
+
+      function go_main(){
+        location.replace("index.html")
+    } 
